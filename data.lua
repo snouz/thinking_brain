0b6c687a9559c2c6119c57fe09236a2ed55830f6
snouz <snooz-x@hotmail.com>
2025-03-22 22:20:49 +0100

    initial empty mod

---
+++ data.lua
@@ -0,0 +1,9 @@
+require ("prototypes.technology")
+require ("prototypes.items")
+require ("prototypes.recipes")
+--require ("prototypes.entities")
+
+require ("sound-util")
+
+require ("prototypes.compatibility")
+
---
+++ prototypes/items.lua
@@ -0,0 +1,8 @@
+local item_sounds = require("__base__.prototypes.item_sounds")
+local item_tints = require("__base__.prototypes.item-tints")
+local space_age_item_sounds = require("__space-age__.prototypes.item_sounds")
+--[[
+data:extend({
+
+})
+]]
